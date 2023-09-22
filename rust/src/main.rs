187be52b9b51be13661e51f0f8e68f63011a6f52
Praveen Perera <praveen@avencera.com>
2023-09-22 19:19:01 -0400

    WIP

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,12 @@
+use rayon::slice::ParallelSliceMut;
+use rustc_data_structures::fx::FxHashMap;
 use serde::{Deserialize, Serialize};
-use serde_json::from_str;
-use std::collections::HashMap;
+use std::io::BufReader;
+
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Hash)]
 struct Post {
@@ -18,22 +24,32 @@ struct RelatedPosts<'a> {
 }
 
 fn main() {
-    let json_str = std::fs::read_to_string("../posts.json").unwrap();
-    let posts: Vec<Post> = from_str(&json_str).unwrap();
+    let file = std::fs::File::open("posts.json").unwrap();
+    let reader = BufReader::with_capacity(1024 * 512, file);
 
-    let mut post_tags_map: HashMap<&String, Vec<&Post>> = HashMap::new();
+    let posts: Vec<Post> = serde_json::from_reader(reader).unwrap();
+
+    let mut post_tags_map: FxHashMap<&String, Vec<&Post>> = FxHashMap::default();
 
     for post in &posts {
         for tag in &post.tags {
-            post_tags_map.entry(tag).or_default().push(post);
+            post_tags_map
+                .entry(tag)
+                .and_modify(|v| v.push(post))
+                .or_insert_with(|| {
+                    let mut v = Vec::with_capacity(post.tags.len());
+                    v.push(post);
+                    v
+                });
         }
     }
 
     let mut related_posts: Vec<RelatedPosts> = Vec::with_capacity(posts.len());
+    let mut related_posts_map: FxHashMap<&Post, i8> = FxHashMap::default();
+
+    related_posts_map.reserve(posts.len());
 
     for post in posts.iter() {
-        let mut related_posts_map: HashMap<&Post, i8> = HashMap::new();
-
         for tag in &post.tags {
             if let Some(tag_posts) = post_tags_map.get(tag) {
                 for other_post in tag_posts {
@@ -44,12 +60,11 @@ fn main() {
             }
         }
 
-        let mut related_posts_for_post: Vec<_> = related_posts_map.into_iter().collect();
+        let mut related_posts_for_post: Vec<_> = Vec::with_capacity(posts.len());
+        related_posts_for_post.extend(related_posts_map.drain());
 
-        // related_videos_for_video.sort_unstable_by_key(|&(_, count)| -count);
-        related_posts_for_post.sort_by(|post_a, post_b| post_b.1.cmp(&post_a.1));
-
-        related_posts_for_post.truncate(5);
+        // related_posts_for_post.sort_unstable_by_key(|&(_, count)| -count);
+        related_posts_for_post.par_sort_unstable_by(|post_a, post_b| post_b.1.cmp(&post_a.1));
 
         related_posts.push(RelatedPosts {
             _id: &post._id,
@@ -57,11 +72,13 @@ fn main() {
             related: related_posts_for_post
                 .into_iter()
                 .map(|(post, _)| post)
+                .take(5)
                 .collect(),
         });
     }
 
     // Write the result to a JSON file.
-    let json_str = serde_json::to_string(&related_posts).unwrap();
-    std::fs::write("../related_posts_rust.json", json_str).unwrap();
+    let json = serde_json::to_vec(&related_posts).unwrap();
+
+    std::fs::write("../related_posts.json", json).unwrap();
 }
